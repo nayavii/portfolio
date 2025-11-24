38b8f41c07a5fd7f13e620eda412d124355b2f7a
Anya Alesik <100134026+nayavii@users.noreply.github.com>
2025-11-24 20:12:06 +0100

    created font mixin

---
+++ src/styles/Common.ts
@@ -0,0 +1,22 @@
+import { theme } from "./Theme";
+
+type CommonFontPropsType = {
+  weight?: number;
+  color?: string;
+  lineHeight?: number;
+  FmaxSize?: number;
+  FminSize?: number;
+};
+
+export const font = ({
+  weight,
+  color,
+  lineHeight,
+  FmaxSize,
+  FminSize,
+}: CommonFontPropsType) => `
+  font-weight: ${weight || 400};
+  color: ${color || theme.colors.text};  
+  line-height: ${lineHeight || 1.2};
+  font-size: min(calc( (100vw - 414px)/(1440 - 414) * (${FmaxSize} - ${FminSize}) + ${FminSize}px),  ${FmaxSize}px);
+`;
