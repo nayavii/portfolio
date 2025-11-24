c02c25c09c1c0f3fbc6901ae2e9d87fc7986406b
Anya Alesik <100134026+nayavii@users.noreply.github.com>
2025-11-24 18:11:46 +0100

    added breakpoitns

--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -1,11 +1,14 @@
-
-
 export const theme = {
   colors: {
     bg: "#17151B",
     cardBg: "#2A2631",
     accent: "#F3C4D6",
-    text: "#FFF8FB", 
-    gradient: "linear-gradient(90deg, #FBC8D4 0%, #9795F0 100%)"
+    text: "#FFF8FB",
+    gradient: "linear-gradient(90deg, #FBC8D4 0%, #9795F0 100%)",
+  },
+
+  media: {
+    tablet: "screen and (max-width: 768px)",
+    mobile: "screen and (max-width: 576px)",
   },
 };
