feba44868e1a970ede6ad7ba071c8d3e506e53b4
Anya Alesik <100134026+nayavii@users.noreply.github.com>
2025-11-24 20:50:53 +0100

    added adaptive for Section Title

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,11 +1,13 @@
 import styled from "styled-components";
 import { theme } from "../styles/Theme";
+import { font } from "../styles/Common";
 
 export const SectionTitle = styled.h2`
-  font-weight: 600;
+  ${font({ weight: 600, lineHeight: "26px", color: theme.colors.accent })}
   font-size: 24px;
-  line-height: 26px;
-  letter-spacing: 4%;
-  color: ${theme.colors.accent};
   margin-bottom: 30px;
+
+  @media ${theme.media.mobile} {
+    margin-bottom: 20px;
+  }
 `;
