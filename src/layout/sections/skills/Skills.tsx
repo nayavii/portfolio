3822cd4b91e7d84ff9c01205c26fcb89cff109e3
Anya Alesik <100134026+nayavii@users.noreply.github.com>
2025-12-02 19:06:31 +0100

    change skill data

--- src/layout/sections/skills/Skills.tsx
+++ src/layout/sections/skills/Skills.tsx
@@ -11,16 +11,16 @@ const skillsData = [
   { iconId: "cssSvg", title: "css" },
   { iconId: "javascriptSvg", title: "javascript" },
   { iconId: "typescriptSvg", title: "typescript" },
+  { iconId: "reactjsSvg", title: "react js" },
+  { iconId: "reactnativeSvg", title: "react native" },
+  { iconId: "reduxSvg", title: "redux" },
+  { iconId: "gitSvg", title: "git" },
+  { iconId: "styledcomponentSvg", title: "styled component" },
   { iconId: "mongoSvg", title: "mongo bd" },
   { iconId: "postgresqlSvg", title: "postgresql" },
   { iconId: "jestSvg", title: "jest" },
   { iconId: "nestjsSvg", title: "nest js" },
   { iconId: "dockerSvg", title: "docker" },
-  { iconId: "reactjsSvg", title: "react js" },
-  { iconId: "reactnativeSvg", title: "react native" },
-  { iconId: "styledcomponentSvg", title: "styled component" },
-  { iconId: "reduxSvg", title: "redux" },
-  { iconId: "gitSvg", title: "git" },
 ];
 
 export const Skills: React.FC = () => {
@@ -28,7 +28,7 @@ export const Skills: React.FC = () => {
     <S.Skills>
       <Container>
         <SectionTitle>Skills</SectionTitle>
-        <FlexWrapper wrap="wrap" justify="space-between" gap="60px 150px">
+        <FlexWrapper wrap="wrap" justify="center" gap="60px 150px">
           <Fade cascade damping={0.1} triggerOnce>
             {skillsData.map(({ iconId, title }, index) => (
               <Skill key={index} iconId={iconId} title={title} />
